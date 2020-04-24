bd6ccbc3692beb11017a3d683c7e73a6d3962f87
Aaron Delago <02delaar@rgtfo-me.it>
2020-04-24 09:34:18 +0200

    1 commit

---
+++ Zahlensysteme_4BEL_Gruppe_B/Zahlensysteme_4BEL_Gruppe_B/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Zahlensysteme_4BEL_Gruppe_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
