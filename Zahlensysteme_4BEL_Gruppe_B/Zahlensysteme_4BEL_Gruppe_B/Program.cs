1503f2b66476675744c6ae51459a236f2aead2ca
Aaron Delago <02delaar@rgtfo-me.it>
2020-04-24 09:45:56 +0200

    Menüstruktur 1 Versuch

--- Zahlensysteme_4BEL_Gruppe_B/Zahlensysteme_4BEL_Gruppe_B/Program.cs
+++ Zahlensysteme_4BEL_Gruppe_B/Zahlensysteme_4BEL_Gruppe_B/Program.cs
@@ -10,6 +10,26 @@ namespace Zahlensysteme_4BEL_Gruppe_B
     {
         static void Main(string[] args)
         {
+            int Eingabe = 0;
+
+            while (Eingabe != 9)
+            {
+                Console.WriteLine("Wilkommen im Menü!\n");
+                Console.WriteLine("Wählen sie mit der daneben angegebenen Zahl das Programm:\n");
+                Console.WriteLine("1.   Umrechnen in Basis 10!");
+                Console.WriteLine("2.   Umrechnung in Basis 16!");
+                Console.WriteLine("3.   Ende\n");
+                Console.Write("Nummer der Berechnung: ");
+
+                while (!Int32.TryParse(Console.ReadLine(), out Eingabe) || Eingabe < 1 || Eingabe > 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FEHLER. Bitte wählen sie eine der 7 Varianten aus!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+            Console.ReadKey();
+
         }
     }
 }
